f9724f8b8356741e07ef4863ff5a38e77cd83af4
ken-katsuno <54093925+ken-katsuno@users.noreply.github.com>
2025-02-04 20:17:18 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+self.addEventListener("install", (event) => {
+  console.log("Service Worker installing.");
+  event.waitUntil(
+    caches.open("timer-app").then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/style.css",
+        "/script.js",
+        "/manifest.json",
+        "/icon-192.png",
+        "/icon-512.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
